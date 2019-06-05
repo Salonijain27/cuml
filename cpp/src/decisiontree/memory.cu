1dcce05f73aaa5d8d229d1b09df9557a21f8b601
Vishal Mehta <vishalm@nvidia.com>
2019-06-05 11:31:54 +0200

    fixed bugs

--- cpp/src/decisiontree/memory.cu
+++ cpp/src/decisiontree/memory.cu
@@ -28,7 +28,7 @@ template<class T>
 TemporaryMemory<T>::TemporaryMemory(const ML::cumlHandle_impl& handle, int N, int Ncols, int maxstr, int n_unique, int n_bins, const int split_algo):ml_handle(handle)
 	{
 
-		std::cout << Inside the constructor \n" << std::flush;
+		std::cout << "Inside the constructor \n" << std::flush;
 		std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n" << std::flush;
 		
 		//Assign Stream from cumlHandle
@@ -39,7 +39,7 @@ TemporaryMemory<T>::TemporaryMemory(const ML::cumlHandle_impl& handle, int N, in
 		h_hist = new MLCommon::host_buffer<int>(handle.getHostAllocator(), stream, n_hist_elements);
 		d_hist = new MLCommon::device_buffer<int>(handle.getDeviceAllocator(), stream, n_hist_elements);
 		nrowsleftright = new MLCommon::host_buffer<int>(handle.getHostAllocator(), stream, 2);
-		std::cout << after setting the h_dist and the d_hist \n" << std::flush;
+		std::cout << "after setting the h_dist and the d_hist \n" << std::flush;
 		int extra_elements = Ncols;
 		int quantile_elements = (split_algo == ML::SPLIT_ALGO::GLOBAL_QUANTILE) ? extra_elements : 1;
 
@@ -76,14 +76,16 @@ TemporaryMemory<T>::TemporaryMemory(const ML::cumlHandle_impl& handle, int N, in
 
 	}
 
-void TemporaryMemory::print_info()
+template<class T>
+void TemporaryMemory<T>::print_info()
 	{
 		std::cout <<" Inside the print_info function  \n" << std::flush;
 		std::cout << " Total temporary memory usage--> "<< ((double)totalmem/ (1024*1024)) << "  MB" << std::endl;
 		return;
 	}
 
-TemporaryMemory::~TemporaryMemory()
+template<class T>
+TemporaryMemory<T>::~TemporaryMemory()
 	{
 
 		std::cout <<" inside the distructor  \n" << std::flush;
@@ -133,4 +135,3 @@ TemporaryMemory::~TemporaryMemory()
 
 	}
 
-};
